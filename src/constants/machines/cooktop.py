2991ef7a1039ee83066babf88eb80b7176ef59df
Divya Roshinkumar <68938920+divz-tech@users.noreply.github.com>
2025-05-19 19:41:46 +1200

    feat: update cooktop constants (#3)
    Co-authored-by: Divya Roshinkumar <divya.roshinkumar@halter.co.nz>

--- src/constants/machines/cooktop.py
+++ src/constants/machines/cooktop.py
@@ -1,43 +1,168 @@
 from openapi_client.models import CooktopEnum
 from constants.fuel_stats import FuelTypeEnum
 from constants.machines.machine_info import MachineInfoMap
+from openapi_client.models.location_enum import LocationEnum
 
-# kwh_per_day are from values in Machines!D196:G196
+# kwh_per_day are from values in Machines!B263:L266
 COOKTOP_INFO: MachineInfoMap = {
     CooktopEnum.GAS: {
-        "kwh_per_day": 1.94,
         "fuel_type": FuelTypeEnum.NATURAL_GAS,
+        "per_location": {
+            LocationEnum.OTHER_TERRITORIES: {
+                "kwh_per_day": 2.20,
+            },
+            LocationEnum.VICTORIA: {
+                "kwh_per_day": 2.14,
+            },
+            LocationEnum.NEW_SOUTH_WALES: {
+                "kwh_per_day": 2.21,
+            },
+            LocationEnum.NORTHERN_TERRITORY: {
+                "kwh_per_day": 2.32,
+            },
+            LocationEnum.AUSTRALIAN_CAPITAL_TERRITORY: {
+                "kwh_per_day": 2.07,
+            },
+            LocationEnum.TASMANIA: {
+                "kwh_per_day": 2.34,
+            },
+            LocationEnum.WESTERN_AUSTRALIA: {
+                "kwh_per_day": 2.26,
+            },
+            LocationEnum.SOUTH_AUSTRALIA: {
+                "kwh_per_day": 2.35,
+            },
+            LocationEnum.QUEENSLAND: {
+                "kwh_per_day": 2.17,
+            },
+            LocationEnum.OVERSEAS: {
+                "kwh_per_day": 0,
+            },
+        },
     },
     CooktopEnum.LPG: {
-        "kwh_per_day": 1.94,
         "fuel_type": FuelTypeEnum.LPG,
+        "per_location": {
+            LocationEnum.OTHER_TERRITORIES: {
+                "kwh_per_day": 2.20,
+            },
+            LocationEnum.VICTORIA: {
+                "kwh_per_day": 2.14,
+            },
+            LocationEnum.NEW_SOUTH_WALES: {
+                "kwh_per_day": 2.21,
+            },
+            LocationEnum.NORTHERN_TERRITORY: {
+                "kwh_per_day": 2.32,
+            },
+            LocationEnum.AUSTRALIAN_CAPITAL_TERRITORY: {
+                "kwh_per_day": 2.07,
+            },
+            LocationEnum.TASMANIA: {
+                "kwh_per_day": 2.34,
+            },
+            LocationEnum.WESTERN_AUSTRALIA: {
+                "kwh_per_day": 2.26,
+            },
+            LocationEnum.SOUTH_AUSTRALIA: {
+                "kwh_per_day": 2.35,
+            },
+            LocationEnum.QUEENSLAND: {
+                "kwh_per_day": 2.17,
+            },
+            LocationEnum.OVERSEAS: {
+                "kwh_per_day": 0,
+            },
+        },
     },
     CooktopEnum.ELECTRIC_RESISTANCE: {
-        "kwh_per_day": 0.83,
         "fuel_type": FuelTypeEnum.ELECTRICITY,
+        "per_location": {
+            LocationEnum.OTHER_TERRITORIES: {
+                "kwh_per_day": 0.94,
+            },
+            LocationEnum.VICTORIA: {
+                "kwh_per_day": 0.92,
+            },
+            LocationEnum.NEW_SOUTH_WALES: {
+                "kwh_per_day": 0.95,
+            },
+            LocationEnum.NORTHERN_TERRITORY: {
+                "kwh_per_day": 0.99,
+            },
+            LocationEnum.AUSTRALIAN_CAPITAL_TERRITORY: {
+                "kwh_per_day": 0.88,
+            },
+            LocationEnum.TASMANIA: {
+                "kwh_per_day": 1.00,
+            },
+            LocationEnum.WESTERN_AUSTRALIA: {
+                "kwh_per_day": 0.97,
+            },
+            LocationEnum.SOUTH_AUSTRALIA: {
+                "kwh_per_day": 1.00,
+            },
+            LocationEnum.QUEENSLAND: {
+                "kwh_per_day": 0.93,
+            },
+            LocationEnum.OVERSEAS: {
+                "kwh_per_day": 0,
+            },
+        },
     },
     CooktopEnum.ELECTRIC_INDUCTION: {
-        "kwh_per_day": 0.75,
         "fuel_type": FuelTypeEnum.ELECTRICITY,
+        "per_location": {
+            LocationEnum.OTHER_TERRITORIES: {
+                "kwh_per_day": 0.85,
+            },
+            LocationEnum.VICTORIA: {
+                "kwh_per_day": 0.83,
+            },
+            LocationEnum.NEW_SOUTH_WALES: {
+                "kwh_per_day": 0.86,
+            },
+            LocationEnum.NORTHERN_TERRITORY: {
+                "kwh_per_day": 0.90,
+            },
+            LocationEnum.AUSTRALIAN_CAPITAL_TERRITORY: {
+                "kwh_per_day": 0.80,
+            },
+            LocationEnum.TASMANIA: {
+                "kwh_per_day": 0.91,
+            },
+            LocationEnum.WESTERN_AUSTRALIA: {
+                "kwh_per_day": 0.87,
+            },
+            LocationEnum.SOUTH_AUSTRALIA: {
+                "kwh_per_day": 0.91,
+            },
+            LocationEnum.QUEENSLAND: {
+                "kwh_per_day": 0.84,
+            },
+            LocationEnum.OVERSEAS: {
+                "kwh_per_day": 0,
+            },
+        },
     },
 }
 
-# From 'Product prices'!D25:D27
+# From 'Product prices'!B22:F26
 COOKTOP_UPFRONT_COST = {
     CooktopEnum.GAS: {
-        "item_price": 1022,
-        "install_cost": 630,
+        "item_price": 700,
+        "install_cost": 400,
     },
     CooktopEnum.LPG: {
-        "item_price": 1022,
-        "install_cost": 630,
+        "item_price": 700,
+        "install_cost": 400,
     },
     CooktopEnum.ELECTRIC_RESISTANCE: {
-        "item_price": 879,
-        "install_cost": 288,
+        "item_price": 600,
+        "install_cost": 400,
     },
     CooktopEnum.ELECTRIC_INDUCTION: {
-        "item_price": 1430,
-        "install_cost": 1265,
+        "item_price": 1400,
+        "install_cost": 600,
     },
 }
