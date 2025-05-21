624060d8eadc1be1d238a431b12e9d8dca76ae7e
Divya Roshinkumar <68938920+divz-tech@users.noreply.github.com>
2025-05-21 17:10:00 +1200

    feat: update clean household to have locs (#23)
    Co-authored-by: Divya Roshinkumar <divya.roshinkumar@halter.co.nz>

--- src/utils/clean_household.py
+++ src/utils/clean_household.py
@@ -1,18 +1,19 @@
 from constants.machines.vehicles import VEHICLE_AVG_KMS_PER_WEEK
 from openapi_client.models.household import Household
+from openapi_client.models.location_enum import LocationEnum
 from openapi_client.models.vehicle import Vehicle
 
 
 def clean_household(household: Household) -> Household:
-    household.vehicles = [clean_vehicle(v) for v in household.vehicles]
+    household.vehicles = [clean_vehicle(v, household.location) for v in household.vehicles]
     return household
 
 
-def clean_vehicle(vehicle: Vehicle) -> Vehicle:
+def clean_vehicle(vehicle: Vehicle, location: LocationEnum) -> Vehicle:
     return vehicle.copy(
         update={
             "kms_per_week": (
-                round(VEHICLE_AVG_KMS_PER_WEEK)
+                round(VEHICLE_AVG_KMS_PER_WEEK[location])
                 if vehicle.kms_per_week is None
                 else vehicle.kms_per_week
             )
