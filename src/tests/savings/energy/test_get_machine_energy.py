09fac75a8d5cef6b491be8ed4c1a4fe13bfb5c6f
Divya Roshinkumar <68938920+divz-tech@users.noreply.github.com>
2025-05-21 13:11:24 +1200

    test: get_machine_energy to include locs (#14)
    Co-authored-by: Divya Roshinkumar <divya.roshinkumar@halter.co.nz>

--- src/tests/savings/energy/test_get_machine_energy.py
+++ src/tests/savings/energy/test_get_machine_energy.py
@@ -44,46 +44,52 @@ mock_energy_weekly = {
 
 class TestGetEnergyPerDay(TestCase):
     mock_appliance_info: MachineInfoMap = {
-        CooktopEnum.GAS: {"kwh_per_day": 10.0, "fuel_type": FuelTypeEnum.NATURAL_GAS},
+        CooktopEnum.GAS: {"fuel_type": FuelTypeEnum.NATURAL_GAS, "per_location": {
+            LocationEnum.NEW_SOUTH_WALES: {"kwh_per_day": 10.0},
+        }},
         SpaceHeatingEnum.ELECTRIC_HEAT_PUMP: {
-            "kwh_per_day": 5.0,
             "fuel_type": FuelTypeEnum.ELECTRICITY,
+            "per_location": {
+                LocationEnum.NEW_SOUTH_WALES: {"kwh_per_day": 5.0},
+            },
         },
     }
 
     def test_get_energy_per_day(self):
-        assert get_energy_per_day(CooktopEnum.GAS, self.mock_appliance_info) == {
-            FuelTypeEnum.NATURAL_GAS: 10.0
+        assert get_energy_per_day(CooktopEnum.GAS, self.mock_appliance_info, LocationEnum.NEW_SOUTH_WALES) == {
+            FuelTypeEnum.NATURAL_GAS: 10
         }
         assert get_energy_per_day(
-            SpaceHeatingEnum.ELECTRIC_HEAT_PUMP, self.mock_appliance_info
+            SpaceHeatingEnum.ELECTRIC_HEAT_PUMP, self.mock_appliance_info, LocationEnum.NEW_SOUTH_WALES
         ) == {FuelTypeEnum.ELECTRICITY: 5.0}
 
     def test_get_energy_per_day_multiple_fuels(self):
-        assert get_energy_per_day(VehicleFuelTypeEnum.HYBRID, VEHICLE_INFO) == {
-            FuelTypeEnum.PETROL: 31.4 * 0.7,
-            FuelTypeEnum.ELECTRICITY: 7.324 * 0.3,
+        assert get_energy_per_day(VehicleFuelTypeEnum.HYBRID, VEHICLE_INFO, LocationEnum.NEW_SOUTH_WALES) == {
+            FuelTypeEnum.PETROL: 36.7 * 0.7,
+            FuelTypeEnum.ELECTRICITY: 9.4 * 0.3,
         }
-        assert get_energy_per_day(VehicleFuelTypeEnum.PLUG_IN_HYBRID, VEHICLE_INFO) == {
-            FuelTypeEnum.PETROL: 31.4 * 0.6,
-            FuelTypeEnum.ELECTRICITY: 7.324 * 0.4,
+        assert get_energy_per_day(VehicleFuelTypeEnum.PLUG_IN_HYBRID, VEHICLE_INFO, LocationEnum.NEW_SOUTH_WALES) == {
+            FuelTypeEnum.PETROL: 36.7 * 0.6,
+            FuelTypeEnum.ELECTRICITY: 9.4 * 0.4,
         }
 
     def test_get_energy_per_day_handles_missing_kwh_per_day(self):
         mock_appliance_info = {
             CooktopEnum.GAS: {
-                "kwh_per_day": None,
                 "fuel_type": FuelTypeEnum.NATURAL_GAS,
+                "per_location": {
+                    LocationEnum.NEW_SOUTH_WALES: {"kwh_per_day": None},
+                },
             }
         }
         with self.assertRaises(ValueError):
-            get_energy_per_day(CooktopEnum.GAS, mock_appliance_info)
+            get_energy_per_day(CooktopEnum.GAS, mock_appliance_info, LocationEnum.NEW_SOUTH_WALES)
 
     def test_get_energy_per_day_handles_invalid_machine_type(self):
         invalid_machine_type = CooktopEnum.GAS
         invalid_mock_appliance_info = {}
         with self.assertRaises(KeyError):
-            get_energy_per_day(invalid_machine_type, invalid_mock_appliance_info)
+            get_energy_per_day(invalid_machine_type, invalid_mock_appliance_info, LocationEnum.NEW_SOUTH_WALES)
 
 
 @patch(
@@ -99,27 +105,35 @@ class TestGetEnergyPerPeriod:
         get_energy_per_period(
             mock_household.space_heating,
             SPACE_HEATING_INFO,
+            LocationEnum.NEW_SOUTH_WALES,
             1,
             PeriodEnum.DAILY,
-            LocationEnum.OTAGO,
         )
         mock_get_energy_per_day.assert_called_once_with(
-            SpaceHeatingEnum.WOOD, SPACE_HEATING_INFO, 1, LocationEnum.OTAGO
+            SpaceHeatingEnum.WOOD, SPACE_HEATING_INFO, LocationEnum.NEW_SOUTH_WALES, 1
         )
 
     def test_it_calls_get_energy_per_day_correctly_with_defaults(
         self, mock_get_energy_per_day, _
     ):
-        get_energy_per_period(mock_household.space_heating, SPACE_HEATING_INFO)
+        get_energy_per_period(
+            mock_household.space_heating,
+            SPACE_HEATING_INFO,
+            LocationEnum.NEW_SOUTH_WALES,
+        )
         mock_get_energy_per_day.assert_called_once_with(
-            SpaceHeatingEnum.WOOD, SPACE_HEATING_INFO, None, None
+            SpaceHeatingEnum.WOOD, SPACE_HEATING_INFO, LocationEnum.NEW_SOUTH_WALES, None
         )
 
     def test_it_calls_scale_daily_to_period_correctly(
         self, _, mock_scale_daily_to_period
     ):
         get_energy_per_period(
-            mock_household.cooktop, COOKTOP_INFO, 1, PeriodEnum.WEEKLY
+            mock_household.cooktop,
+            COOKTOP_INFO,
+            LocationEnum.NEW_SOUTH_WALES,
+            1,
+            PeriodEnum.WEEKLY,
         )
         mock_scale_daily_to_period.assert_called_once_with(
             mock_energy_daily[FuelTypeEnum.ELECTRICITY], PeriodEnum.WEEKLY
@@ -128,7 +142,11 @@ class TestGetEnergyPerPeriod:
     def test_it_calls_scale_daily_to_period_correctly_with_default(
         self, _, mock_scale_daily_to_period
     ):
-        get_energy_per_period(mock_household.cooktop, COOKTOP_INFO)
+        get_energy_per_period(
+            mock_household.cooktop,
+            COOKTOP_INFO,
+            LocationEnum.NEW_SOUTH_WALES,
+        )
         mock_scale_daily_to_period.assert_called_once_with(
             mock_energy_daily[FuelTypeEnum.ELECTRICITY], PeriodEnum.DAILY
         )
@@ -143,13 +161,21 @@ class TestGetEnergyPerPeriod:
             }
         ]
 
-        get_energy_per_period(mock_household.cooktop, COOKTOP_INFO)
+        get_energy_per_period(
+            mock_household.cooktop,
+            COOKTOP_INFO,
+            LocationEnum.NEW_SOUTH_WALES,
+        )
         assert len(mock_scale_daily_to_period.call_args_list) == 2
         mock_scale_daily_to_period.assert_any_call(2, PeriodEnum.DAILY)
         mock_scale_daily_to_period.assert_any_call(3, PeriodEnum.DAILY)
 
     def test_it_returns_energy_per_period(self, _, __):
-        result = get_energy_per_period(mock_household.space_heating, SPACE_HEATING_INFO)
+        result = get_energy_per_period(
+            mock_household.space_heating,
+            SPACE_HEATING_INFO,
+            LocationEnum.NEW_SOUTH_WALES,
+        )
         assert result == mock_energy_weekly
 
 
@@ -185,7 +211,7 @@ class TestGetEnergyPerPeriod:
 class TestGetTotalApplianceEnergy:
     def test_it_combines_fuel_types_correctly(self, mock_get_energy_per_period):
         result = get_total_appliance_energy(
-            mock_household, PeriodEnum.DAILY, LocationEnum.OTAGO
+            mock_household, PeriodEnum.DAILY, LocationEnum.NEW_SOUTH_WALES
         )
         expected = {
             FuelTypeEnum.ELECTRICITY: 6,
@@ -225,7 +251,7 @@ class TestGetTotalApplianceEnergy:
             },
         ]
         result = get_total_appliance_energy(
-            mock_household, PeriodEnum.DAILY, LocationEnum.OTAGO
+            mock_household, PeriodEnum.DAILY, LocationEnum.NEW_SOUTH_WALES
         )
         expected = {
             FuelTypeEnum.ELECTRICITY: 6,
@@ -247,24 +273,24 @@ class TestGetTotalApplianceEnergy:
     return_value=mock_energy_daily[FuelTypeEnum.ELECTRICITY],
 )
 class TestGetOtherAppliancesEnergyPerPeriod:
-    energy = 0.34 + 4.05 + 2.85
+    energy = 0.77 + 5.32 + 3.40 + 1.01
 
     def test_it_calls_scale_energy_by_occupancy_correctly(
         self, mock_scale_energy_by_occupancy, mock_scale_daily_to_period
     ):
-        get_other_appliances_energy_per_period(1, PeriodEnum.WEEKLY)
+        get_other_appliances_energy_per_period(LocationEnum.NEW_SOUTH_WALES, 1, PeriodEnum.WEEKLY)
         mock_scale_energy_by_occupancy.assert_called_once_with(self.energy, 1)
 
     def test_it_calls_scale_energy_by_occupancy_correctly_with_default(
         self, mock_scale_energy_by_occupancy, mock_scale_daily_to_period
     ):
-        get_other_appliances_energy_per_period()
+        get_other_appliances_energy_per_period(LocationEnum.NEW_SOUTH_WALES)
         mock_scale_energy_by_occupancy.assert_called_once_with(self.energy, None)
 
     def test_it_calls_scale_daily_to_period_correctly(
         self, mock_scale_energy_by_occupancy, mock_scale_daily_to_period
     ):
-        get_other_appliances_energy_per_period(None, PeriodEnum.WEEKLY)
+        get_other_appliances_energy_per_period(LocationEnum.NEW_SOUTH_WALES, None, PeriodEnum.WEEKLY)
         mock_scale_daily_to_period.assert_called_once_with(
             mock_energy_daily[FuelTypeEnum.ELECTRICITY], PeriodEnum.WEEKLY
         )
@@ -272,46 +298,63 @@ class TestGetOtherAppliancesEnergyPerPeriod:
     def test_it_calls_scale_daily_to_period_correctly_with_default(
         self, mock_scale_energy_by_occupancy, mock_scale_daily_to_period
     ):
-        get_other_appliances_energy_per_period()
+        get_other_appliances_energy_per_period(LocationEnum.NEW_SOUTH_WALES)
         mock_scale_daily_to_period.assert_called_once_with(
             mock_energy_daily[FuelTypeEnum.ELECTRICITY], PeriodEnum.DAILY
         )
 
     def test_it_returns_energy_per_period(self, _, __):
-        assert get_other_appliances_energy_per_period(1, PeriodEnum.WEEKLY) == {
+        assert get_other_appliances_energy_per_period(
+            LocationEnum.NEW_SOUTH_WALES, 1, PeriodEnum.WEEKLY
+        ) == {
             FuelTypeEnum.ELECTRICITY: 17.5
         }
-        assert get_other_appliances_energy_per_period() == {
+        assert get_other_appliances_energy_per_period(
+            LocationEnum.NEW_SOUTH_WALES
+        ) == {
             FuelTypeEnum.ELECTRICITY: 17.5
         }
 
+
+def assert_any_call_with_approx(mock, expected_arg0, expected_arg1, tol=1e-6):
+    for call in mock.call_args_list:
+        arg0, arg1 = call.args
+        if abs(arg0 - expected_arg0) < tol and arg1 == expected_arg1:
+            return
+    raise AssertionError(f"No call found with approx({expected_arg0}) and {expected_arg1}")
 
 class TestGetVehicleEnergyPerDay(TestCase):
     # kWh per day for average mileage
-    petrol = 31.4
-    ev = 7.324
+    petrol = 36.7
+    ev = 9.4
 
-    expected_weighted_energy_daily_petrol = petrol * (250 / 210)
-    expected_weighted_energy_daily_ev = ev * (250 / 210)
+    expected_weighted_energy_daily_petrol = petrol * (250 / 253.4)
+    expected_weighted_energy_daily_ev = ev * (250 / 253.4)
 
     def test_it_calculates_daily_energy_for_one_petrol_car(self):
-        result = get_vehicle_energy([mock_vehicle_petrol])
-        assert result == {
-            FuelTypeEnum.PETROL: self.expected_weighted_energy_daily_petrol
-        }
+        result = get_vehicle_energy(
+            [mock_vehicle_petrol], LocationEnum.NEW_SOUTH_WALES
+        )
+        assert result[FuelTypeEnum.PETROL] == pytest.approx(self.expected_weighted_energy_daily_petrol)
 
     def test_it_calculates_daily_energy_for_one_diesel_car(self):
-        result = get_vehicle_energy([mock_vehicle_diesel])
-        expected = 22.8 * 50 / 210
-        assert result == {FuelTypeEnum.DIESEL: expected}
+        result = get_vehicle_energy(
+            [mock_vehicle_diesel], LocationEnum.NEW_SOUTH_WALES
+        )
+        expected = 29 * 50 / 253.4
+        assert result[FuelTypeEnum.DIESEL] == pytest.approx(expected)
 
     def test_it_calculates_daily_energy_for_one_ev(self):
-        result = get_vehicle_energy([mock_vehicle_ev])
-        assert result == {FuelTypeEnum.ELECTRICITY: self.ev * (250 / 210)}
+        result = get_vehicle_energy(
+            [mock_vehicle_ev], LocationEnum.NEW_SOUTH_WALES
+        )
+        assert result[FuelTypeEnum.ELECTRICITY] == pytest.approx(self.ev * (250 / 253.4))
 
     def test_it_calculates_daily_energy_for_one_hybrid(self):
-        result = get_vehicle_energy([mock_vehicle_hev])
-        weighting_factor = 150 / 210
+        result = get_vehicle_energy(
+            [mock_vehicle_hev], LocationEnum.NEW_SOUTH_WALES
+        )
+        weighting_factor = 150 / 253.4
         expected = {
             FuelTypeEnum.PETROL: self.petrol * 0.7 * weighting_factor,
             FuelTypeEnum.ELECTRICITY: self.ev * 0.3 * weighting_factor,
@@ -319,13 +362,16 @@ class TestGetVehicleEnergyPerDay(TestCase):
         assert result == expected
 
     def test_it_calculates_daily_energy_for_one_plugin_hybrid(self):
-        result = get_vehicle_energy([mock_vehicle_phev])
-        weighting_factor = 175 / 210
+        result = get_vehicle_energy(
+            [mock_vehicle_phev], LocationEnum.NEW_SOUTH_WALES
+        )
+        weighting_factor = 175 / 253.4
         expected = {
             FuelTypeEnum.PETROL: self.petrol * 0.6 * weighting_factor,
             FuelTypeEnum.ELECTRICITY: self.ev * 0.4 * weighting_factor,
         }
-        assert result == expected
+        assert result[FuelTypeEnum.PETROL] == pytest.approx(expected[FuelTypeEnum.PETROL])
+        assert result[FuelTypeEnum.ELECTRICITY] == pytest.approx(expected[FuelTypeEnum.ELECTRICITY])
 
     def test_it_combines_vehicles_correctly(self):
         result = get_vehicle_energy(
@@ -335,33 +381,43 @@ class TestGetVehicleEnergyPerDay(TestCase):
                 mock_vehicle_ev,
                 mock_vehicle_hev,
                 mock_vehicle_phev,
-            ]
+            ],
+            LocationEnum.NEW_SOUTH_WALES,
         )
         expected = {
-            FuelTypeEnum.PETROL: (31.4 * (250 / 210))  # petrol
-            + (self.petrol * 0.7 * (150 / 210))  # HEV
-            + (self.petrol * 0.6 * (175 / 210)),  # PHEV
-            FuelTypeEnum.DIESEL: (22.8 * (50 / 210)),  # diesel
-            FuelTypeEnum.ELECTRICITY: (7.324 * (250 / 210))  # EV
-            + (self.ev * 0.3 * (150 / 210))  # HEV
-            + (self.ev * 0.4 * (175 / 210)),  # PHEV
+            FuelTypeEnum.PETROL: (36.7 * (250 / 253.4))  # petrol
+            + (self.petrol * 0.7 * (150 / 253.4))  # HEV
+            + (self.petrol * 0.6 * (175 / 253.4)),  # PHEV
+            FuelTypeEnum.DIESEL: (29 * (50 / 253.4)),  # diesel
+            FuelTypeEnum.ELECTRICITY: (9.4 * (250 / 253.4))  # EV
+            + (self.ev * 0.3 * (150 / 253.4))  # HEV
+            + (self.ev * 0.4 * (175 / 253.4)),  # PHEV
         }
-        assert result == expected
+        assert result[FuelTypeEnum.PETROL] == pytest.approx(expected[FuelTypeEnum.PETROL])
+        assert result[FuelTypeEnum.DIESEL] == pytest.approx(expected[FuelTypeEnum.DIESEL])
+        assert result[FuelTypeEnum.ELECTRICITY] == pytest.approx(expected[FuelTypeEnum.ELECTRICITY])
 
     @patch(
         "savings.energy.get_machine_energy.scale_daily_to_period",
     )
     def test_it_calls_scale_daily_to_period_correctly(self, mock_scale_daily_to_period):
 
-        get_vehicle_energy([mock_vehicle_ev, mock_vehicle_petrol], PeriodEnum.WEEKLY)
+        get_vehicle_energy(
+            [mock_vehicle_ev, mock_vehicle_petrol],
+            LocationEnum.NEW_SOUTH_WALES,
+            PeriodEnum.WEEKLY,
+        )
 
         assert len(mock_scale_daily_to_period.call_args_list) == 2
-        mock_scale_daily_to_period.assert_any_call(
+        assert_any_call_with_approx(
+            mock_scale_daily_to_period,
             self.expected_weighted_energy_daily_petrol,
             PeriodEnum.WEEKLY,
         )
-        mock_scale_daily_to_period.assert_any_call(
-            self.expected_weighted_energy_daily_ev, PeriodEnum.WEEKLY
+        assert_any_call_with_approx(
+            mock_scale_daily_to_period,
+            self.expected_weighted_energy_daily_ev,
+            PeriodEnum.WEEKLY
         )
 
     @patch(
@@ -370,27 +426,38 @@ class TestGetVehicleEnergyPerDay(TestCase):
     def test_it_calls_scale_daily_to_period_correctly_with_default(
         self, mock_scale_daily_to_period
     ):
-        get_vehicle_energy([mock_vehicle_ev, mock_vehicle_petrol])
+        get_vehicle_energy(
+            [mock_vehicle_ev, mock_vehicle_petrol],
+            LocationEnum.NEW_SOUTH_WALES,
+        )
         assert len(mock_scale_daily_to_period.call_args_list) == 2
-        mock_scale_daily_to_period.assert_any_call(
+        assert_any_call_with_approx(
+            mock_scale_daily_to_period,
             self.expected_weighted_energy_daily_petrol,
             PeriodEnum.DAILY,
         )
-        mock_scale_daily_to_period.assert_any_call(
-            self.expected_weighted_energy_daily_ev, PeriodEnum.DAILY
+        assert_any_call_with_approx(
+            mock_scale_daily_to_period,
+            self.expected_weighted_energy_daily_ev,
+            PeriodEnum.DAILY
         )
 
     def test_it_returns_energy_with_default_period(self):
-        result = get_vehicle_energy([mock_vehicle_ev, mock_vehicle_petrol])
+        result = get_vehicle_energy(
+            [mock_vehicle_ev, mock_vehicle_petrol],
+            LocationEnum.NEW_SOUTH_WALES,
+        )
 
-        assert result == {
+        assert pytest.approx(result) == {
             FuelTypeEnum.PETROL: self.expected_weighted_energy_daily_petrol,
             FuelTypeEnum.ELECTRICITY: self.expected_weighted_energy_daily_ev,
         }
 
     def test_it_returns_energy_with_specified_period(self):
         result = get_vehicle_energy(
-            [mock_vehicle_ev, mock_vehicle_petrol], PeriodEnum.WEEKLY
+            [mock_vehicle_ev, mock_vehicle_petrol],
+            LocationEnum.NEW_SOUTH_WALES,
+            PeriodEnum.WEEKLY,
         )
         expected = {
             FuelTypeEnum.PETROL: self.expected_weighted_energy_daily_petrol * 7,
